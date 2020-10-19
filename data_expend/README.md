68c9ac5ad318e10953747fe90be17dca5e3cad4d
allen <zxcabc968123@gmail.com>
2020-10-19 13:09:19 +0800

    201019

--- data_expend/README.md
+++ data_expend/README.md
@@ -2,5 +2,7 @@
 1. -> open lable program --TrainDIR (img folder) --SaveDIR (save folder .txt) 
 python3 labeling_locatev2.py --TrainDIR origin_img/ --SaveDIR origin_data/ 
 2. -> translate .txt to .csv
+3. -> use check_data.py show locate img example: python3 check_data.py --TrainDIR /origin_data/train_locate_2020-10-19_11_52_49_.csv
+
 
 
--- data_expend/check_data.py
+++ data_expend/check_data.py
@@ -11,9 +11,14 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import pandas as pd
+import argparse
 import math
 
-data_csv='/home/allen/dl_grasp/src/data_expend/origin_data/data_2020-10-12.csv'
+parser = argparse.ArgumentParser()
+parser.add_argument('--TrainDIR', type=str, default='', help='path to Trainingdata')
+FLAGS = parser.parse_args()
+data_csv='/home/allen/dl_grasp/src/data_expend'+FLAGS.TrainDIR
+
 
 def pd_read_csv(csvFile):
  
@@ -38,16 +43,27 @@ def pd_read_csv(csvFile):
         data7.append(data_row["width"])
         data8.append(data_row["high"])
     return (data1,data2,data3,data4,data5,data6,data7,data8)
+
+def trans_degree(x,y,degree):
+    degree=float(degree)*math.pi/180
+    l=50
+    f_x=float(x)+l*math.cos(degree)
+    f_y=float(y)-l*math.sin(degree)
+    tmp_x=x-round(f_x)
+    tmp_y=y-round(f_y)
+    return (round(tmp_x),round(tmp_y))
     
-
-
 def main():
     data1,data2,data3,data4,data5,data6,data7,data8=pd_read_csv(data_csv)
-    for i in range(len(data1)-1):
+    for i in range(len(data1)):
+        print('Img :'+data1[i])
+        print('angle :{}'.format(data4[i]))
         image=cv2.imread(data1[i],-1)
         cv2.circle(image, (data2[i],data3[i]), 5, (0,0,255),3)
         cv2.rectangle(image,(data5[i],data6[i]),(data7[i],data8[i])\
             ,(255, 0, 0),2)
+        tmp_x,tmp_y=trans_degree(data2[i],data3[i],data4[i])
+        cv2.line(image,(data2[i]+tmp_x,data3[i]+tmp_y),(data2[i]-tmp_x,data3[i]-tmp_y),(255,0,0),2)
         cv2.imshow('My image',image)
         cv2.waitKey(0)
 if __name__ == "__main__":
